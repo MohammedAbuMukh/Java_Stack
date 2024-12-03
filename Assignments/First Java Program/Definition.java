d8716230837a48e32b7d1397f4e69a13d7b18d5f
MohammedAbuMukh <mohammedabomokh3@gmail.com>
2024-12-03 13:34:48 +0200

    first commit in java stack

---
+++ Assignments/First Java Program/Definition.java
@@ -0,0 +1,7 @@
+public class Definition {
+    public static void main(String[] args) {
+        System.out.println("My Name is Mohammad AbuMukh");
+        System.out.println("I'm 24 years old");
+        System.out.println("My hometown is Tulkarm, Palestine");
+    }
+}
