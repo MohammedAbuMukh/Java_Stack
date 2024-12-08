cb29a65ab7022b4f11998a7571565cb0a4c136ec
MohammedAbuMukh <mohammedabomokh3@gmail.com>
2024-12-08 08:17:00 +0200

    add more assignments

---
+++ Assignments/Puzzle/PuzzleJava.java
@@ -0,0 +1,44 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PuzzleJava {
+
+    // getTenRolls
+    // Write a method that will generate and return an array with 10 random numbers between 1 and 20 inclusive. 
+    // To get a random integer, you can use the nextInt method of the Random class. Random Class documentation
+    public ArrayList<Integer> getTenRolls() {
+    ArrayList<Integer> randomNum = new ArrayList<Integer>();
+    Random randMachine = new Random();
+    for (Integer i = 0; i<10; i++){
+    randomNum.add(randMachine.nextInt(20));
+}
+return randomNum;
+
+}
+public String getRandomLetter(){
+        Random rand = new Random();
+        String [] letters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        
+            return letters[rand.nextInt(26)];
+
+    
+}
+
+public String generatePassword(){
+    Random rand = new Random();
+    String password = "";
+        for (int i = 0; i<8; i++){
+            password = password + getRandomLetter();
+        }
+        return password;
+}
+public ArrayList<String> setPassword(int length){
+    ArrayList<String> passwordSet = new ArrayList<String>();
+    
+    for (int i = 0; i < length; i++){
+        passwordSet.add(generatePassword());
+    }
+    return passwordSet;
+}
+}
---
+++ Assignments/Puzzle/TestPuzzleJava.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Random;
+public class TestPuzzleJava {
+    
+	public static void main(String[] args) {
+		PuzzleJava generator = new PuzzleJava();
+		ArrayList<Integer> randomRolls = generator.getTenRolls();
+		String randomLetters = generator.getRandomLetter();
+		String randomPassword = generator.generatePassword();
+		ArrayList<String> newPassword = generator.setPassword(8);
+		System.out.println(randomRolls);
+		System.out.println("the Random Letter is: " + randomLetters);
+		System.out.println("Password: " + randomPassword);
+		System.out.println("New Password: " + newPassword );
+    	
+	}
+}
